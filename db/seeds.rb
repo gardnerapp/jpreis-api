32ca79ef6955b0f69dbcce7419b85b8ca004d1b2
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-12-01 14:22:23 -0500

    Finish seeding DataAPI

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,5 +73,127 @@ contact?',
                   queryparameters: 'filtertype={filtertype} &pagenum={pagenum}
 &numrecsperpage={numrecsperpage}'
                 },
+                { # TODO
+                  name: 'Fetch Point Contacts List For Enterprise Contact',
+                  endpoint: '/svc/bw/data/enterprise-contact/{contactid}/poc',
+                  queryparameters: ''
+                },
+                {
+                  name: 'Fetch Logon Session Information',
+                  endpoint: '/svc/bw/data/user/logonsession?',
+                  queryparameters: 'querystr={logicalexpression}
+&numrecsperpage={number} &pagenum={number}
+&timezone={timezone} &timeformat={timeformat}'
+                },
+                {
+                  name: 'Fetch License Information',
+                  endpoint: '/svc/bw/data/license?',
+                  queryparameters: 'querystr={logicalexpression} &numrecsperpage={number}
+&pagenum={number}'
+                },
+                {
+                  name: 'Fetch Location Information',
+                  endpoint: '/svc/bw/data/location?querystr=',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Account Policy',
+                  endpoint: '/svc/bw/data/account-policy?querystr=',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Codec Profile',
+                  endpoint: '/svc/bw/data/codec-profile?querystr=',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Authentication Domain',
+                  endpoint: '/svc/bw/data/authentication-domain?
+querystr=',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch User Groups',
+                  endpoint: '/svc/bw/data/group?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Hunt Groups',
+                  endpoint: '/svc/bw/data/group/hunt?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Roles',
+                  endpoint: '/svc/bw/data/role?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch All Users From A User Group',
+                  endpoint: '/svc/bw/data/group/member?',
+                  queryparameters: 'querystr={logicalexpression}
+&numrecsperpage={numrecsperpage}
+&pagenum={pagenum}
+&lastuserid={lastuserid}'
+                },
+                {
+                  name: 'Fetch Key Codes Information',
+                  endpoint: '/svc/bw/data/key-codes?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Zone Inventory Information',
+                  endpoint: '/svc/bw/data/inventory/zone?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Device Inventory Information',
+                  endpoint: '/svc/bw/data/inventory/device?',
+                  queryparameters: 'querystr={logicalexpression} &numrecsperpage={number}
+&pagenum={number}'
+                },
+                {
+                  name: 'Fetch SBC Inventory Information',
+                  endpoint: '/svc/bw/data/inventory/sessionbordercontroller?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Voice Recorder Inventory Information',
+                  endpoint: '/svc/bw/data/inventory/voicerecorder?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                {
+                  name: 'Fetch Voice Recorder Inventory Information',
+                  endpoint: '/svc/bw/data/inventory/voicerecorder?',
+                  queryparameters: 'querystr={logicalexpression}'
+                },
+                { # TODO fix
+                  name: 'Create a Data Status Monitor Resource',
+                  endpoint: '/svc/bw/data/status-monitor',
+                  queryparameters: ''
+                },
+                { # TODO fix
+                  name: 'Update a Data Status Monitor Resource',
+                  endpoint: '/svc/bw/data/status-monitor/',
+                  queryparameters: 'monitorID{monitorId}'
+                },
+                { # TODO fix
+                  name: 'Fetch Details of a Data Status Monitor Resource',
+                  endpoint: '/svc/bw/data/status-monitor/',
+                  queryparameters: 'monitorID{monitorId}'
+                },
+                { # TODO fix
+                  name: 'Fetch Resource Updates By Polling the Data Status Monitor',
+                  endpoint: '/svc/bw/data/status-monitor/event/',
+                  queryparameters: 'monitorID{monitorId}'
+                },
+                { # TODO fix
+                  name: 'Delete A Data Status Monitor Resource',
+                  endpoint: '/svc/bw/data/status-monitor/',
+                  queryparameters: 'monitorID{monitorId}'
+                },
+
+
+
+
 
                ])
