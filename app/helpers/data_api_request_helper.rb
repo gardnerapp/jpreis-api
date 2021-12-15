c78c995ede6b3a2e3f9208b253d5850ed09227ee
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-12-15 11:30:29 -0500

    remove eval from data api request helper

--- app/helpers/data_api_request_helper.rb
+++ app/helpers/data_api_request_helper.rb
@@ -1,20 +1,19 @@
 module DataApiRequestHelper
   include RequestHelper
-  require 'faraday'
 
-  def make_request(params, data_api_token)
-    header_token data_api_token
-    base_url = "https://#{params[:ip]}#{params[:endpoint]}"
+  def make_request(params, token)
+    create_req_args params, nil, token
+    @method = :get
     params.delete(:endpoint)
     params.delete(:ip)
-    params.each_pair do |k,v|
-      if base_url.include? k
-        base_url = base_url.gsub /{#{k}}/, v
+    params.each_pair do |k, v|
+      if @base_url.include? k
+        @base_url = @base_url.gsub(/{#{k}}/, v)
         params.delete k
       end
     end
-    params.delete_if { |_k,v| v.empty? }
-    params.each_pair { |k,v| base_url.insert(-1, "#{k}#{v}") }
-    response = Faraday.get(base_url, nil, @headers)
+    params.delete_if { |_k, v| v.empty? }
+    params.each_pair { |k, v| @base_url.insert(-1, "#{k}#{v}") }
+    send_req
   end
 end
--- app/helpers/reference_api_req_helper.rb
+++ app/helpers/reference_api_req_helper.rb
@@ -3,7 +3,6 @@ module ReferenceApiReqHelper
   require 'faraday'
 
   def make_request(params)
-    #Faraday.get('https://www.google.com')
-    Faraday.get("https://#{params[:ip]}//svc/bw/api?/schema=#{params[:schema]}", HEADERS)
+    Faraday.get("https://#{params[:ip]}//svc/bw/api?/schema=#{params[:schema]}", nil,  HEADERS)
   end
 end
