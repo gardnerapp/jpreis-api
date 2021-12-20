7690426e03bcd7b957522ff6ef7f21cc98146be8
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-12-20 11:40:28 -0500

    add views to data status monitor

--- app/controllers/data_status_monitor_api_controller.rb
+++ app/controllers/data_status_monitor_api_controller.rb
@@ -1,10 +1,14 @@
 class DataStatusMonitorApiController < ApplicationController
   def index
+    @data_calls = DataStatusMonitorApi.all
   end
 
   def show
+    @data = DataStatusMonitorApi.find(params[:id])
   end
 
   def call
+    # add form,xml true == to_method_helper add id
+    # add req methods to helpers
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'data_status_monitor_api/index'
-  get 'data_status_monitor_api/show'
-  get 'call_api/index'
-  get 'call_api/show'
+  root to: 'data_api#index'
 
+  # Authorization Endpoints
   get '/sessions/new', to: 'api_sessions#new'
   post '/sessions', to: 'api_sessions#create'
   get '/sessions/refresh', to: 'api_sessions#refresh'
@@ -11,8 +9,7 @@ Rails.application.routes.draw do
   get '/sessions/terminate', to: 'api_sessions#terminate'
   post '/sessions/terminate', to: 'api_sessions#destroy'
 
-  root to: 'data_api#index'
-
+  # Calling pages
   get '/reference', to: 'reference_api#show'
   post '/reference', to: 'reference_api#create'
 
@@ -28,14 +25,15 @@ Rails.application.routes.draw do
   resources :device_api, only: %i[show index]
   post '/device/call', to: 'device_api#call'
 
+  resources :data_status_monitor_api, only: %i[show index]
+  post '/data_status/call', to: 'data_status_monitor_api#call'
 
-  #
-  # Device, DataStatusMonitor, Monitoring 21 calls left
+
+  # DataStatusMonitor, Monitoring
   #
   # TODO add dropdown burger to full navbar do large font
   # TODO add device to navbar
-  # #TODO add xml param to form
+  #
   # TODO go through todos
-  # merge device-ui to data-status-monitor-api
 
 end
