4a53305575596d188a72f8991c2e583989c643e5
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2022-03-16 14:39:04 -0400

    Add active storage migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_153353) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_16_183824) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.integer "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "call_apis", force: :cascade do |t|
     t.string "name"
@@ -18,8 +45,8 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.string "method"
     t.string "queryparameters"
     t.string "xmlparameters"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "calls", force: :cascade do |t|
@@ -28,8 +55,8 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.string "req_verb"
     t.integer "resp_status"
     t.text "resp_body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "req", default: false
   end
 
@@ -39,16 +66,16 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.string "queryparameters"
     t.string "method"
     t.string "xmlparameters"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "data_apis", force: :cascade do |t|
     t.string "name"
     t.string "endpoint"
     t.text "queryparameters"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "data_status_monitor_apis", force: :cascade do |t|
@@ -57,8 +84,8 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.string "method"
     t.string "queryparameters"
     t.string "xmlparameters"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "device_apis", force: :cascade do |t|
@@ -67,8 +94,8 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.string "method"
     t.string "queryparameters"
     t.string "xmlparameters"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "management_apis", force: :cascade do |t|
@@ -77,8 +104,8 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.string "queryparameters"
     t.string "xmlparameters"
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "admin", default: false
   end
 
@@ -87,8 +114,10 @@ ActiveRecord::Schema.define(version: 2022_03_14_153353) do
     t.json "queryparameters"
     t.json "querystrparameters"
     t.string "guidelines"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
